419cce6ffce88f848454bb61900f8f61d302d94a
Azerty-Labs <75258525+azerty-labs@users.noreply.github.com>
2024-08-01 23:57:57 +0200

    remove boilerplate

--- src/speech_to_text.py
+++ src/speech_to_text.py
@@ -1,16 +1,5 @@
-# ------------------------------------------------------------------------------
-# Speech to Text App
-# ------------------
-#
-# This Streamlit app allows you to convert speech to text from either an uploaded 
-# audio file or a YouTube video.The app uses whisper speech-to-text library 
-# that openai developped.
-#
-# The page and the model integration has been developed by Azerty-Labs
-# 
-# <Copyright 2023 - Azerty-Labs>
-# ------------------------------------------------------------------------------
 import whisper
+
 
 def process_file(filename):
     model = whisper.load_model("base")
